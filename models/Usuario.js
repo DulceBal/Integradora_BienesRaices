03288b55b3ba71597496be0bb31e0bc8b634c313
Dulce Balderas Gomez <230693@utxicotepec.edu.mx>
2024-12-17 01:39:22 -0600

    ACT06
    Actividad 06

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,7 +1,6 @@
-import { DataTypes } from "sequelize"
-import bcrypt from 'bcrypt'
-import db from '../config/db.js'
-
+import { DataTypes } from "sequelize";
+import bcrypt from 'bcrypt';
+import db from '../config/db.js';
 
 const Usuario = db.define('usuarios', {
     nombre: {
@@ -19,11 +18,23 @@ const Usuario = db.define('usuarios', {
     token: {
         type: DataTypes.STRING
     },
-    confirmado: DataTypes.BOOLEAN
+    confirmado: DataTypes.BOOLEAN,
+    foto: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    alias: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    fechaDeNacimiento: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+    }
 }, {
     hooks: {
         beforeCreate: async function (usuario) {
-            const salt = await bcrypt.genSalt(10)
+            const salt = await bcrypt.genSalt(10);
             usuario.password = await bcrypt.hash(usuario.password, salt);
         }
     },
@@ -34,12 +45,12 @@ const Usuario = db.define('usuarios', {
             }
         }
     }
-})
+});
 
-//Metodos personalizados
+// Métodos personalizados
 
 Usuario.prototype.verificarPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
-}
+};
 
-export default Usuario
+export default Usuario;
